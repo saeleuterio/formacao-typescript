057ac91666f80036c6095d3ae49b3daebceb5af4
saeleuterio <saeleuterio@hotmail.com>
2024-11-26 17:48:33 -0300

    up

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -3,6 +3,25 @@ import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 3000;
 
+function debitar(valor: number): void {
+    if (valor <= 0){
+        throw new Error("O valor a ser debitado deve ser maior que zero!");
+    }
+    if (valor > saldo) {
+        throw new Error("Saldo insuficiente!");
+    }
+
+    saldo -= valor;
+}
+
+function depositar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("O valor a ser depositado deve ser maior que zero!");
+    }
+
+    saldo += valor;
+}
+
 const Conta = {
     getSaldo() {
         return saldo;
@@ -14,17 +33,16 @@ const Conta = {
 
     registrarTransacao(novaTransacao: Transacao): void {
         if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-            saldo += novaTransacao.valor;
+            depositar(novaTransacao.valor);
         }
         else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-            saldo -= novaTransacao.valor;
+            debitar(novaTransacao.valor);
         }
         else {
-            alert("Tipo de Transação é inválido!");
-            return;
+            throw new Error("Tipo de Transação é inválido!");
         }
-        console.log(novaTransacao);
 
+        console.log(novaTransacao);
     }
 }
 
